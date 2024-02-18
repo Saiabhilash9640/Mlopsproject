df6709f2df9feea86c8f2c3c9c59d2acdb763834
Sai Abhilash <saiabhilash9640@gmail.com>
2024-02-18 13:05:08 +0530

    structure updated

--- template.py
+++ template.py
@@ -5,9 +5,9 @@ from pathlib import Path
 list_of_files=[
     '.github/workflows/.gitkeep',
     'src/__init__.py',
-    'src/components/__init__.py'
+    'src/components/__init__.py',
     'src/components/data_ingestion.py',
-    'src/components/data_transformation.py'
+    'src/components/data_transformation.py',
     'src/components/model_trainer.py',
     'src/components/model_evaluation.py',
     'src/pipeline/__init__.py',
@@ -16,7 +16,7 @@ list_of_files=[
     'src/utils/__init__.py',
     'src/utils/utils.py',
     'src/logger/logging.py',
-    'src/exception/exception'
+    'src/exception/exception.py',
     'tests/unit/__init__.py',
     'tests/integration/__init__.py',
     'init_setup.sh',
